41fd7d47a208cd18fc24c6f4d6bb4df5ef39ef91
copparihollmann <copparihollmann@gmail.com>
2025-08-13 18:40:11 +0200

    Update runtime plugin to include samples instead of runtime folder

--- iree_runtime_plugin.cmake
+++ iree_runtime_plugin.cmake
@@ -2,4 +2,4 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake")
 
 set(IREE_ITA_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/runtime ita-runtime)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/samples ita-samples)
